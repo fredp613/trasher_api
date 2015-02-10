aa9cc41eaea0e4bd753e17380eec14521e9511e0
fredp613 <fredp613@gmail.com>
2015-02-09 21:32:26 -0500

    opts

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
-    @user = User.create(params[:user])
+    @user = User.create(sign_up_params)
     if @user.save
       render :json => {:state => {:code => 0}, :data => @user }
     else
