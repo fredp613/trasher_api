f2a00023c29f14e8010d6a44d62a6af7ba73b47d
fredp613 <fredp613@gmail.com>
2015-02-09 21:25:44 -0500

    opts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:sessions => "sessions"}
+  devise_for :users, :controllers => {:sessions => "sessions", :registrations=>"registrations"}
   resources :temp_images
 
   resources :trash_images
