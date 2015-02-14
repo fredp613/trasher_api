b215c9eb093ec11c733c2080b5bda0265d376a26
fredp613 <fredp613@gmail.com>
2015-02-14 13:48:49 -0500

    json fixes user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,9 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.create(sign_up_params)
     if @user.save
-      render :json => {:state => {:code => 0}, :data => @user }
+      render :json => { :user => @user }
     else
-      render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} }
+      render :json => { :messages => @user.errors.full_messages }
     end
 
   end
