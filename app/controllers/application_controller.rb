04a94bedd169ddac3d5af2a4ef01ab33bcc14015
fredp613 <fredp613@gmail.com>
2015-02-21 13:27:04 -0500

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,12 @@ class ApplicationController < ActionController::Base
   def authenticate_user_from_token!
     user_email = request.headers["X-API-EMAIL"].presence
     user_auth_token = request.headers["X-API-TOKEN"].presence
-    user = User.find_by_email(user_email).first
+    user = user_email && User.find_by_email(user_email).first
 
     # Notice how we use Devise.secure_compare to compare the token
     # in the database with the token given in the params, mitigating
     # timing attacks.
+
     if user && Devise.secure_compare(user.authentication_token, user_auth_token)
       sign_in(user, store: false)
     end
