5bc10219d5ebea9fa0e490fe1e7acf5a2498c8b9
fredp613 <fredp613@gmail.com>
2015-02-22 15:48:34 -0500

    registration json fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def create
-    @user = User.create(sign_up_params)
-    if @user.save
-      render :json => { :state_code => 0, :user => @user }
-    else
-      render :json => { :state_code => 1,  :messages => @user.errors.full_messages }
-    end
-  end
+  def create    
+  respond_to do |format|
+        format.html {
+          super
+        }
+        format.json {
+
+          User.create(sign_up_params)
+          if @user.save
+			      render :json => { :state_code => 0, :user => @user }
+			    else
+			      render :json => { :state_code => 1,  :messages => @user.errors.full_messages }
+			    end
+        }
+      end
 end
