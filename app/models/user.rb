cbd951d81f1517948bb59bc6a362b6668bbb5cb5
fredp613 <fredp613@gmail.com>
2015-02-22 19:29:47 -0500

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  def reset_authentication_token!
+  def self.reset_authentication_token!
     self.authentication_token = generate_authentication_token
   end
 end
