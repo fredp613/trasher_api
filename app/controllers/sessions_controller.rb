c352d1aa7a5ea37831524df4e75e3a80fd3f4588
fredp613 <fredp613@gmail.com>
2015-02-22 21:44:36 -0500

    t

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@
 
 class SessionsController < Devise::SessionsController
   skip_before_filter :authenticate_user!, :only => [:create, :new, :destroy]
-  # skip_before_filter :verify_signed_out_user, :only => [:destroy]
+  skip_before_filter :verify_signed_out_user, :only => [:destroy]
   # skip_before_filter :authenticate_user_from_token!, :only => [:destroy]
   respond_to :json
 
