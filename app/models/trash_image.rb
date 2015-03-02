58abdbb9344dba973b4291b086e3a815a6e64543
fredp613 <fredp613@gmail.com>
2015-03-01 22:08:56 -0500

    test

--- app/models/trash_image.rb
+++ app/models/trash_image.rb
@@ -1,3 +1,5 @@
+require 'stringio'
+
 class TrashImage < ActiveRecord::Base
   belongs_to :trash
 
@@ -10,25 +12,26 @@ class TrashImage < ActiveRecord::Base
 	self.remove_trash_image!
   end
 
-  #  def image_data=(data)
-  #   # decode data and create stream on them
-  #   io = CarrierStringIO.new(Base64.decode64(data))
+   def image_data=(data)
+    # decode data and create stream on them
+    io = CarrierStringIO.new(Base64.decode64(data))
 
-  #   # this will do the thing (photo is mounted carrierwave uploader)
-  #   self.trash_image = io
-  # end
+    # this will do the thing (photo is mounted carrierwave uploader)
+    self.trash_image = io
+  end
 
 
 end
 
-# class CarrierStringIO < StringIO
-#   def original_filename
-#     # the real name does not matter
-#     "photo.jpeg"
-#   end
+class CarrierStringIO < StringIO
+  def original_filename
+    # the real name does not matter
+    "photo.jpeg"
+  end
 
-#   def content_type
-#     # this should reflect real content type, but for this example it's ok
-#     "image/jpeg"
-#   end
-# end 
+  def content_type
+    # this should reflect real content type, but for this example it's ok
+    "image/jpeg"
+  end
+end 
+
