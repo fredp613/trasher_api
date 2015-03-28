f86c3eba32bc54b96e49e41c7b102bf3ed117156
fredp613 <fredp613@gmail.com>
2015-03-28 12:07:33 -0400

    t

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < Devise::SessionsController
   respond_to :json
 
   
-  
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   
 
     def new
